904f1622afb25461ac39a7a819bdb394a33d36bc
Sophia  Sales <69023582+SophaiSales@users.noreply.github.com>
2020-09-14 15:35:21 -0300

    ajustes finais

--- Contador.asm
+++ Contador.asm
@@ -2,15 +2,15 @@
 ;02/09/2020-TDM2
 
         org 0000h             ;capacidade memoria 4bits
-			
+ini:			
         mov a,#00h            ;zerando o acomulador 
+
 volta:
         mov p1,a              ;mover acomulador para p1
-		inc a                 ;incrementar acomulador 
-		cjne a,#05h,volta     ;compara e salta acomulador
-
-volta1:	
-        mov p1,a              ;mover acomulador para p1 
-		djnz acc,volta1       ;decrementar acomulador ate chega 0
-		jmp volta             ;retorna para volta
-        end		              ;fim
+		add a,#01h            ;incrementar acomulador 
+        da a  
+        cjne a,#60H,volta     ;compara e salta acomulador
+		
+		jmp ini               ;recomeçar novamente  
+		end                   ;fim
+          
